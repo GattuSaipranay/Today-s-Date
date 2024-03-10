d182647122a3e345ac1ab15cf60a9f4fa183a92e
GattuSaipranay <121419602+GattuSaipranay@users.noreply.github.com>
2024-03-10 17:59:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require("express");
 const app = express();
-const addDays = require("date-fns/addDays");
-
 app.get("/", (request, response) => {
-  let dateTime = new Date();
-  let date = addDays(
-    new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate()),
-    100
-  );
+  const dateTime = new Date();
+
   response.send(
-    `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    `${dateTime.getDate()}-${dateTime.getMonth() + 1}-${dateTime.getFullYear()}`
   );
 });
-
-app.listen(3000);
 module.exports = app;
